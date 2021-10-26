63e40870f95e94c8f96e8ce26a92e2a4da33a5bf
[Georgy Fufaev] <[georijf@bk.ru]>
2021-10-27 00:39:55 +0500

    Add project files.

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Здравствуйте, как Вас зовут?");
+        //    string name = Console.ReadLine();
+        //    Console.WriteLine($"Очень приятно, {name}! Кстати, сегодня {DateTime.Now}");
+        //}
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+
+
+
+    }
+}
