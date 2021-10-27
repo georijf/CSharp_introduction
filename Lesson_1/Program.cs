356c496b49273ac7934e8f7fb2380eb3a6079e62
Georgy <georijf@bk.ru>
2021-10-27 09:52:43 +0500

    new feature

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +18,7 @@ namespace Lesson_1
 
 
 
-
+        // from GitKraken
 
         static void Main(string[] args)
         {
