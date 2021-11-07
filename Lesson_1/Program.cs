38eae44a2ccd5ce14704de8fc4e4da4108e7dba6
[Georgy Fufaev] <[georijf@bk.ru]>
2021-11-07 21:09:50 +0500

    Homework for Lesson 4

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -195,56 +195,209 @@ namespace Lesson_1
         #endregion
 
         #region Lesson 3
+        //static void Main(string[] args)
+        //{
+        //    int nextString = 0;
+        //    int[,] array = new int[5, 5];
+        //    for (int i = 0; i < array.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < array.GetLength(1); j++)
+        //        {
+        //            array[i, j] = nextString + 1;
+        //            Console.WriteLine($"{new string(' ', nextString)}{array[i, j]}");
+        //            nextString++;
+        //        }
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    var phoneBook = new string[,]
+        //    {
+        //    {"Иванов А.П.", "89475688156"},
+        //    {"Сидоров П.К.", "89487264871"},
+        //    {"Петров М.У.", "89743542194"},
+        //    {"Конев М.Н.", "87358941573"},
+        //    {"Анисова Д.С.", "89753495815"},
+        //    };
+        //    for (var i = 0; i < phoneBook.GetLength(0); i++)
+        //    {
+        //        for (var j = 0; j < phoneBook.GetLength(1); j++)
+        //            Console.Write(phoneBook[i, j] + "\t");
+        //        Console.WriteLine();
+        //    }
+        //    Console.ReadKey();
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите строку");
+        //    string str = Console.ReadLine();
+        //    Console.Write("Ваша строка в обратном порядке:");
+        //    string reverseStr = "";
+        //    for (int i = str.Length - 1; i >= 0; i--)
+        //    {
+        //        reverseStr += str[i]; // переворачиваем строку
+        //    }
+        //    Console.WriteLine(reverseStr);
+        //    Console.ReadKey();
+        //}
+        #endregion
+
+        #region Lesson 4
+        enum Seasons
+        {
+            None = 0,
+            Winter = 1,
+            Spring = 2,
+            Summer = 3,
+            Autumn = 4
+        }
+
         static void Main(string[] args)
         {
-            int nextString = 0;
-            int[,] array = new int[5, 5];
-            for (int i = 0; i < array.GetLength(0); i++)
+            Console.WriteLine("Задание №1");
+            Console.WriteLine(GetFullName("Сергей", "Гудовщиков", "Константинович"));
+            Console.WriteLine(GetFullName("Максим", "Селезнёв", "Олегович"));
+            Console.WriteLine(GetFullName("Константин", "Попов", "Михаилович"));
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №2");
+            Console.Write("Введите набор чисел, разделенных пробелом: ");
+            Console.WriteLine(GetSum(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №3");
+            Console.Write("Введите порядковый номер месяца: ");
+            int number;
+            do
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number < 1 || number > 12)
                 {
-                    array[i, j] = nextString + 1;
-                    Console.WriteLine($"{new string(' ', nextString)}{array[i, j]}");
-                    nextString++;
+                    Console.Write("Ошибка. Введите число от 1 до 12: ");
+                }
+            } while (number < 1 || number > 12);
+            Seasons season = GetSeason(number);
+            Console.WriteLine("Время года: " + GetSeasonStr(season));
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №4");
+            Console.Write("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число Фибоначчи: " + Fibonachi(n));
+            Console.ReadKey();
+
+         
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic) // Метод задания №1
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+
+
+        static int GetSum(string inputLine) //Метод задания №2
+        {
+            int sum = 0;
+
+            string[] numbers = inputLine.Split(' ');
+            string number = " ";
+            for (int i = 0; i <= inputLine.Length; i++)
+            {
+                if (i == inputLine.Length || inputLine[i] == ' ')
+                {
+                    if (number != " ")
+                    {
+                        sum += Convert.ToInt32(number);
+                        number = " ";
+                    }
+                }
+                else
+                {
+                    number += inputLine[i];
                 }
             }
+            return sum;
         }
 
-        static void Main(string[] args)
+
+        static Seasons GetSeason(int number) //Метод задания №3
         {
-            var phoneBook = new string[,]
+            Seasons season = Seasons.None;
+            switch (number)
             {
-            {"Иванов А.П.", "89475688156"},
-            {"Сидоров П.К.", "89487264871"},
-            {"Петров М.У.", "89743542194"},
-            {"Конев М.Н.", "87358941573"},
-            {"Анисова Д.С.", "89753495815"},
-            };
-            for (var i = 0; i < phoneBook.GetLength(0); i++)
-            {
-                for (var j = 0; j < phoneBook.GetLength(1); j++)
-                    Console.Write(phoneBook[i, j] + "\t");
-                Console.WriteLine();
+                case 12:
+                case 1:
+                case 2:
+                    season = Seasons.Winter;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = Seasons.Spring;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = Seasons.Summer;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = Seasons.Autumn;
+                    break;
             }
-            Console.ReadKey();
+            return season;
+        }
+        static string GetSeasonStr(Seasons season)
+        {
+            string seasonStr = "";
+            switch (season)
+            {
+                case Seasons.Winter:
+                    seasonStr = "Зима";
+                    break;
+                case Seasons.Spring:
+                    seasonStr = "Весна";
+                    break;
+                case Seasons.Summer:
+                    seasonStr = "Лето";
+                    break;
+                case Seasons.Autumn:
+                    seasonStr = "Осень";
+                    break;
+            }
+            return seasonStr;
         }
 
-        static void Main(string[] args)
+
+        static int Fibonachi(int n) //метод задания №4
         {
-            Console.Write("Введите строку");
-            string str = Console.ReadLine();
-            Console.Write("Ваша строка в обратном порядке:");
-            string reverseStr = "";
-            for (int i = str.Length - 1; i >= 0; i--)
+            return Fibonachi(n, out _);
+        }
+        static int Fibonachi(int n, out int f1)
+        {
+            f1 = 0;
+            if (n == 0)
             {
-                reverseStr += str[i]; // переворачиваем строку
+                return 0;
             }
-            Console.WriteLine(reverseStr);
-            Console.ReadKey();
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int f2; //Число Фибоначчи n - 2
+                f1 = Fibonachi(n - 1, out f2); //Число Фибоначчи n - 1
+                return f1 + f2;
+            }
         }
         #endregion
     }
 }
+        
 
 
 
